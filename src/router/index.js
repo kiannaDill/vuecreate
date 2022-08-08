63de8d96673a15fc60e0cef9a210a461a268069b
汤财鑫 <T18893482913@163.com>
2022-08-08 16:06:02 +0800

    分支2

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,7 @@ import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'home',
     component: HomeView
@@ -16,7 +15,10 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import( /* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }, {
+    path: '/branch',
+    component: () => import('../views/branchView.vue')
   }
 ]
 
---
+++ src/views/branchView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="branch">
+        <h1>111111</h1>
+    </div>
+</template>
+<script>
+export default {
+        
+    }
+</script>
